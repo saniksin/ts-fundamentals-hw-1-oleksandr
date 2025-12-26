1bd9f5e633fddce68dc6c397f6eb7c0182a0c666
Oleksandr Makedon <rbkrutoi@gmail.com>
2025-12-26 16:25:01 +0100

    finish task

--- src/book.ts
+++ src/book.ts
@@ -1,26 +1,61 @@
 // TODO: імпортуй потрібні типи з ./types
 // import type { BookId, Genre, LoanStatus } from "./types";
 
+import type { BookId, Genre, LoanStatus } from "./types";
+
 export class Book {
   // TODO: додай типи до властивостей
-  id;
-  title;
-  author;
-  year;
-  genre;
 
-  status;
-  borrowedBy;
+  // public fields
+  public id: BookId;
+  public title: string;
+  public author: string;
+  public year: number;
+  public genre: Genre;
+
+  // private fields
+  private status: LoanStatus = "available";
+  private borrowedBy: string | null = null;
 
   // TODO: реалізуй конструктор з параметром opts
-  constructor(opts) {}
+  constructor(opts: { id: BookId; title: string; author: string; year: number; genre: Genre }) {
+    // public fields
+    this.id = opts.id;
+    this.title = opts.title;
+    this.author = opts.author;
+    this.year = opts.year;
+    this.genre = opts.genre;
+    this.status = "available";
+    this.borrowedBy = null;
+  }
+
+
 
   // TODO: методи відповідно до ТЗ
-  getStatus() {}
+  getStatus(): LoanStatus {
+    return this.status;
+  }
 
-  markBorrowed(personName) {}
+  markBorrowed(personName: string): void {
+    if (this.status === "borrowed") {
+      throw new Error(`Already borrowed by ${this.borrowedBy}`);
+    }
+    this.status = "borrowed";
+    this.borrowedBy = personName;
+  }
 
-  markReturned() {}
+  markReturned() {
+    if (this.status === "available") {
+      throw new Error("Already available");
+    }
+    this.status = "available";
+    this.borrowedBy = null;
+  }
 
-  getInfo() {}
+  getInfo() {
+    if (this.status === "available") {
+      return `${this.title} — ${this.author} (${this.year}), ${this.genre} [Available]`;
+    }
+    return `${this.title} — ${this.author} (${this.year}), ${this.genre} [Borrowed by ${this.borrowedBy}]`;
+  }
 }
--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,9 @@
 // - BookId: string
 // - Genre: один із літералів "fiction" | "science" | "history" | "fantasy" | "dystopian"
 // - LoanStatus: "available" | "borrowed"
+
+type BookId = string;
+type Genre = "fiction" | "science" | "history" | "fantasy" | "dystopian";
+type LoanStatus = "available" | "borrowed";
+
+export type { BookId, Genre, LoanStatus };
